6d3f542699e56edba9a680c8c785b24985a69eae
leavesster <11785335+leavesster@users.noreply.github.com>
2021-06-12 10:37:42 +0800

    add some config

--- app/src/main/java/com/example/webviewdemo/MainActivity.kt
+++ app/src/main/java/com/example/webviewdemo/MainActivity.kt
@@ -12,16 +12,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         webView = findViewById<WebView>(R.id.webView)
+        webView!!.settings.javaScriptEnabled = true
+//        enable only when you know why you do it
+        webView!!.settings.allowFileAccessFromFileURLs = true
+        webView!!.settings.allowFileAccess = true
         loadLocalHtml()
 //        loadLocalhost()
     }
 
     private fun loadLocalHtml() {
-        webView?.loadUrl("file:///android_asset/index.html")
+        webView!!.loadUrl("file:///android_asset/index.html")
     }
 
     private fun loadLocalhost() {
         // 10.0.2.2 is localhost in Android emulator
-        webView?.loadUrl("http://10.0.2.2:8000")
+        webView!!.loadUrl("http://10.0.2.2:8000")
     }
 }
